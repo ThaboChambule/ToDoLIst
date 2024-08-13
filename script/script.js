d2724920f3935f2225f7cad50549be14137ca846
Thabo <thabochambule1@gmail.com>
2024-08-14 01:17:12 +0200

    generate html in javascript

--- script/script.js
+++ script/script.js
@@ -1,30 +1,25 @@
 document.querySelector(".js-add").addEventListener('click', () => {
-
-    
-
     addTodo();
-    
-
     })
 
 const todoList = [];
 
-let todoListHTML = '';
 
-for(let i=0; i<todoList.length; i++){
-    const todo = todoList[i]
-    const html = `<p>${todo}</p>`
-    todoListHTML += html;
 
-}
 
 const addTodo = () => {
    const inputElement = document.querySelector(".js-input")
    const name = inputElement.value;
 
    todoList.push(name);
-   inputElement.value = '';
+   inputElement.value = ''
+   let todoListHTML = '';
 
+   for(let i=0; i<todoList.length; i++){
+    const todo = todoList[i]
+    const html = `<p>${todo}</p>`
+    todoListHTML += html;
+}
+document.querySelector(".js-list").innerHTML = todoListHTML
    
-
 }
