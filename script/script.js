77d4c057d18219bd4c5bd56c17ab7a8a59ca37ce
Thabo <thabochambule1@gmail.com>
2024-08-14 00:52:15 +0200

    add for loop to loop through the array

--- script/script.js
+++ script/script.js
@@ -9,11 +9,21 @@ document.querySelector(".js-add").addEventListener('click', () => {
 
 const todoList = [];
 
+let todoListHTML = '';
+
+for(let i=0; i<todoList.length; i++){
+    const todo = todoList[i]
+    const html = `<p>${todo}</p>`
+    todoListHTML += html;
+
+}
+
 const addTodo = () => {
    const inputElement = document.querySelector(".js-input")
    const name = inputElement.value;
 
    todoList.push(name);
+   inputElement.value = '';
 
    
 
