de95fff419be5c6ae3d7af74f728fdc8727fae36
Thabo <thabochambule1@gmail.com>
2024-08-14 01:32:09 +0200

    add date selector

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 document.querySelector(".js-add").addEventListener('click', () => {
     addTodo();
     })
-
 const todoList = [];
 
 
@@ -17,9 +16,17 @@ const addTodo = () => {
 
    for(let i=0; i<todoList.length; i++){
     const todo = todoList[i]
-    const html = `<p>${todo}</p>`
+    const html = `
+    <p>${todo}
+    <button onclick="
+    todoList.splice(${i}, 1)
+    addTodo();
+    ">Delete</button>
+    </p>`
     todoListHTML += html;
 }
+
 document.querySelector(".js-list").innerHTML = todoListHTML
-   
+
+
 }
