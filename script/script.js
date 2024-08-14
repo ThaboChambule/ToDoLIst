b4249caa76f718a58fbc9e37b8b7e569cafab1de
Thabo <thabochambule1@gmail.com>
2024-08-14 20:17:54 +0200

    add render function

--- script/script.js
+++ script/script.js
@@ -1,32 +1,37 @@
-document.querySelector(".js-add").addEventListener('click', () => {
-    addTodo();
-    })
+document.querySelector(".js-add").addEventListener("click", () => {
+  addTodo();
+});
+
 const todoList = [];
 
-
-
-
 const addTodo = () => {
-   const inputElement = document.querySelector(".js-input")
-   const name = inputElement.value;
+  const inputElement = document.querySelector(".js-input");
+  const name = inputElement.value;
+  const dateElement = document.querySelector(".js-date");
+  const dueDate = dateElement.value;
 
-   todoList.push(name);
-   inputElement.value = ''
-   let todoListHTML = '';
+  todoList.push({ name, dueDate });
+  inputElement.value = "";
+  dateElement.value = "";
 
-   for(let i=0; i<todoList.length; i++){
-    const todo = todoList[i]
+  renderTodoList();
+};
+
+const renderTodoList = () => {
+    
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
     const html = `
-    <p>${todo}
+    <p>${todo.name} ${todo.dueDate}
     <button onclick="
-    todoList.splice(${i}, 1)
-    addTodo();
+    todoList.splice(${i}, 1);
+    renderTodoList();
     ">Delete</button>
-    </p>`
+    </p>`;
     todoListHTML += html;
-}
+  }
 
-document.querySelector(".js-list").innerHTML = todoListHTML
-
-
-}
+  document.querySelector(".js-list").innerHTML = todoListHTML;
+};
